39df8c28a3185a4d4dc634103a5ec4f25f2d3946
fixtip12 <mwam_fixtip@yahoo.co.jp>
2020-06-13 17:07:48 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,35 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'MicropostsController@index');
+
+Route::namespace('Auth')->group(function () {
+    // ユーザ登録
+    Route::get('signup', 'RegisterController@showRegistrationForm')->name('signup.get');
+    Route::post('signup', 'RegisterController@register')->name('signup.post');
+    // ログイン認証
+    Route::get('login', 'LoginController@showLoginForm')->name('login');
+    Route::post('login', 'LoginController@login')->name('login.post');
+    Route::get('logout', 'LoginController@logout')->name('logout.get');
+});
+
+// ユーザ機能
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    
+    Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', 'UserFollowController@store')->name('user.follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+        Route::get('followings', 'UsersController@followings')->name('users.followings');
+        Route::get('followers', 'UsersController@followers')->name('users.followers');
+    });
+
+    // 追加
+    Route::group(['prefix' => 'microposts/{id}'], function () {
+        Route::post('favorite', 'FavoritesController@store')->name('favorites.favorite');
+        Route::delete('unfavorite', 'FavoritesController@destroy')->name('favorites.unfavorite');
+        Route::get('is_favorite','UsersController@favorites')->name('users.is_favorite');
+    });
+    
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
